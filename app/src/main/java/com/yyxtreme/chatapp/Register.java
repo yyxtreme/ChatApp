9093a0108585ad7814247cc08cb9b67ef68627b8
Yatin <yatinyadav1998@gmail.com>
2017-08-23 22:53:50 +0530

    Chat.java done

--- app/src/main/java/com/yyxtreme/chatapp/Register.java
+++ app/src/main/java/com/yyxtreme/chatapp/Register.java
@@ -1,5 +1,25 @@
 package com.yyxtreme.chatapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Yatin Yadav on 23/08/2017.
  */
